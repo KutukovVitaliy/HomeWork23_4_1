ee2459c1d72abcd595bc2d99d9d0956e6e56f70d
kvitadmin <kutukov@list.ru>
2021-09-03 16:15:51 +0300

    temp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#define WEEK_DAY(a)
+
+#define DAY_1   "Monday"
 int main() {
-    std::cout <<   << std::endl;
+    int number;
+    std::cout << "Input the number of the day of the week: ";
+    std::cin >> number;
+
+    WEEK_DAY(number);
     return 0;
 }
